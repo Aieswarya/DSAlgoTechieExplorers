78c450f8cc4e45b36f9761bead80da0184c7f4d1
PoojaEs <poojaeswarir@gmail.com>
2024-10-07 14:16:02 -0400

    Pooja updated framework code commit

---
+++ DSAlgo_BDD/src/test/java/numpyninja/dsalgo/constants/Constants.java
@@ -0,0 +1,16 @@
+package numpyninja.dsalgo.constants;
+
+public class Constants {
+	
+	// Config properties variables
+	public static String  APP_URL;
+	public static String BROWSER;
+	public static String OS_TYPE;
+	public static String USERNAME;
+	public static String PASSWORD;
+	public static String EXECUTION_ENVIRONMENT;
+	
+	
+	
+
+}
