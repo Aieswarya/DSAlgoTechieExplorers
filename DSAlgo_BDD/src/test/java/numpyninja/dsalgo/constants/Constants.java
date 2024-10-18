c67684b2706668fef71f57cd16d361124179a733
PoojaEs <poojaeswarir@gmail.com>
2024-10-18 16:24:36 -0400

    Pooja update Excel reader and constants,push data excel file

--- DSAlgo_BDD/src/test/java/numpyninja/dsalgo/constants/Constants.java
+++ DSAlgo_BDD/src/test/java/numpyninja/dsalgo/constants/Constants.java
@@ -2,18 +2,21 @@ package numpyninja.dsalgo.constants;
 
 public class Constants {
 	
-	
+	//Values assigned from properties 
 	public static String APP_URL;
 	public static String BROWSER;
 	public static String OS_TYPE;
+	public static String EXECUTION_ENVIRONMENT;
+	
+	//Values assigned from  Excel reader class
+	
 	public static String USERNAME;
 	public static String PASSWORD;
-	public static String EXECUTION_ENVIRONMENT;
 	public static String PYTHON_EDITOR_INPUT;
 	public static String PYTHON_EDITOR_OUTPUT;
 	
-	public static String INCORRECT_USERNAME;
-	public static String INCORRECT_PASSWORD;
+	
+	
 	
 	
 
--- DSAlgo_BDD/src/test/java/numpyninja/dsalgo/utilities/ExcelUtils.java
+++ DSAlgo_BDD/src/test/java/numpyninja/dsalgo/utilities/ExcelUtils.java
@@ -12,47 +12,33 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ExcelUtils {
-	
-	
-	public static  void getLogin() {
-		
-		
+
+	public static void getLogin() {
+
 		try {
 			Constants constants = new Constants();
-			
+
 			FileInputStream file = new FileInputStream(new File("Data.xlsx"));
-			
-			
+
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
-			
+
 			Sheet sheet1 = workbook.getSheet("LOGIN");
-			
+
 			Row row1 = sheet1.getRow(1);
-			
-			Constants.USERNAME=row1.getCell(0).getStringCellValue();
-			constants.PASSWORD=row1.getCell(1).getStringCellValue();
-			// Read Incorrect credential values
-			
-			Row row4 = sheet1.getRow(4);
-			constants.INCORRECT_USERNAME=row4.getCell(0).getStringCellValue();
-			constants.INCORRECT_PASSWORD=row4.getCell(1).getStringCellValue();
-			
+
+			Constants.USERNAME = row1.getCell(0).getStringCellValue();
+			constants.PASSWORD = row1.getCell(1).getStringCellValue();
+
 			Sheet sheet2 = workbook.getSheet("EDITOR");
 			Row row2 = sheet2.getRow(1);
-			constants.PYTHON_EDITOR_INPUT=row2.getCell(0).getStringCellValue();
-			constants.PYTHON_EDITOR_OUTPUT=row2.getCell(1).getStringCellValue();
-			
-			
-			
+			constants.PYTHON_EDITOR_INPUT = row2.getCell(0).getStringCellValue();
+			constants.PYTHON_EDITOR_OUTPUT = row2.getCell(1).getStringCellValue();
+
 			workbook.close();
-		} catch (IOException exception)
-		{
+		} catch (IOException exception) {
 			exception.printStackTrace();
-			}
-		
-		
-	}	
-	
-	
-	
+		}
+
+	}
+
 }
