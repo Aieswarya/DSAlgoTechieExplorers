dfd008d37bbe014d7d0de98ad03cea6e80be16a1
PoojaEs <poojaeswarir@gmail.com>
2024-10-25 19:32:14 -0400

    Pooja push cross browser test runner

--- DSAlgo_BDD/src/test/java/numpyninja/dsalgo/runner/TestRunnerCrossBrowser.java
+++ DSAlgo_BDD/src/test/java/numpyninja/dsalgo/runner/TestRunnerCrossBrowser.java
@@ -26,7 +26,7 @@ public class TestRunnerCrossBrowser extends AbstractTestNGCucumberTests {
 	}
 
 	@Override
-	@DataProvider(parallel = true)
+	@DataProvider(parallel = false)
 	public Object[][] scenarios() {
 		return super.scenarios();
 	}
